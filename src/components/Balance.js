1f5e64d6f930fd666e33ce8070ffc1f4a5b08c0e
gkettani <ghalikettani01@gmail.com>
2022-05-18 20:42:55 +0200

    Handled currency format

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,4 +1,5 @@
-import '../styles/Balance.css'
+import '../styles/Balance.css';
+import { currencyFormatter } from '../utils/currencyFormatter';
 
 export default function Balance(props) {
     let incomes = props.incomes;
@@ -8,16 +9,16 @@ export default function Balance(props) {
     <div className="balance">
         <div>
             <h3 className="balance__title">Your Balance</h3>
-            <span className="balance__value">${incomes - expenses}</span>
+            <span className="balance__value">{currencyFormatter.format(incomes - expenses)}</span>
         </div>
         <div className='balance__info'>
             <div className='balance__info-income'>
                 <p>INCOME</p>
-                <span className='balance__info--green'>${incomes}</span>
+                <span className='balance__info--green'>{currencyFormatter.format(incomes)}</span>
             </div>
             <div className='balance__info-expense'>
                 <p>EXPENSE</p>
-                <span className='balance__info--red'>${expenses}</span>
+                <span className='balance__info--red'>${currencyFormatter.format(expenses)}</span>
             </div>
         </div>
         
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,14 @@
 import '../styles/Transaction.css';
+import { currencyFormatter } from '../utils/currencyFormatter';
 
 export default function Transaction(props) {
     const id = props.id;
-
     return (
         <li className="transaction">
-            <div className="transaction__container">
+            <div className=
+            {'transaction__container ' + (props.value >= 0 ? 'transaction--green' : 'transaction--red')}>
                 <p className="transaction__description"> {props.description} </p>
-                <p className="transaction__value">$ {props.value}</p>
-                <span id="stateColor" className={props.value >= 0 ? 'transaction--green' : 'transaction--red'}/>
+                <p className="transaction__value">{currencyFormatter.format(props.value)}</p>
             </div>
             <button className="delete-btn" onClick={()=>props.onDelete(id)}><i className="uil uil-times-circle"></i></button>
         </li>
